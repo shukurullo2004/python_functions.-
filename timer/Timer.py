cdca20d87c6cbba20eabe8a056e3f1d8f5efd95e
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-06-11 01:35:18 +0900

    s

--- timer/Timer.py
+++ timer/Timer.py
@@ -1,6 +1,13 @@
+
 import time
-myTime = int(input("enter time: "))
-for x in range(myTime, 0, -1):
-    second = x%60
+
+my_time = int(input("Enter the time in seconds: "))
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
-    print(x)
+
+print("TIME'S UP!")
