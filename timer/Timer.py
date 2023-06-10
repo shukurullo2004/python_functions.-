36ce16227fc9e01bea720f16bf133130687f8c53
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-06-11 01:22:17 +0900

    changes

--- timer/Timer.py
+++ timer/Timer.py
@@ -1,6 +1,6 @@
 import time
 myTime = int(input("enter time: "))
-for x in range(myTime, 0):
-    second = x/60
-    time.sleep(4)
+for x in range(myTime, 0, -1):
+    second = x%60
+    time.sleep(1)
     print(x)
